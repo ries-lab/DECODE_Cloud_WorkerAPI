f395fcda57704fd5f5ad4c9f07ab2b359c875ea6
Tomáš Daniš <tomas.danis@cronus.sk>
2023-03-20 12:59:34 +0100

    Fix postgres DB connection logic

--- api/database.py
+++ api/database.py
@@ -4,9 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 from api.settings import database_url
 
-
 engine = create_engine(
-    database_url, connect_args={"check_same_thread": False}
+    database_url, connect_args={"check_same_thread": False} if database_url.startswith("sqlite") else {}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
