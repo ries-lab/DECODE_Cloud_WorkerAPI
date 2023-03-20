41cc2b6575768f1777fbee0adf03cfe1a6185255
nolan1999 <arthur.jaques@hispeed.ch>
2023-03-20 23:48:56 +0100

    Authentification to DB of workerfacing api

--- workerfacing_api/queue.py
+++ workerfacing_api/queue.py
@@ -1,8 +1,16 @@
+import json
+import os
 from workerfacing_api.core.queue import RDSJobQueue
 from workerfacing_api import settings
 
 
-queue = RDSJobQueue(settings.queue_db_url)
+queue_db_url = settings.queue_db_url
+
+# AWS password
+if "QUEUE_DB_SECRET" in os.environ:
+    queue_db_url = queue_db_url.format(json.loads(os.environ["QUEUE_DB_SECRET"])["password"])
+
+queue = RDSJobQueue(queue_db_url)
 queue.create(err_on_exists=False)
 
 
