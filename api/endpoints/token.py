f7efd37dc366edbada80e7355c6772fb1fd858e0
nolan1999 <arthur.jaques@hispeed.ch>
2023-09-15 16:36:59 +0200

    Token post errors

--- api/endpoints/token.py
+++ api/endpoints/token.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 import boto3
 
 from api.schemas.token import TokenLogin, TokenResponse
@@ -11,7 +11,6 @@ router = APIRouter()
 @router.post("/token", response_model=TokenResponse)
 async def token(login: TokenLogin):
     client = boto3.client('cognito-idp')
-
     try:
         # Perform the login using the email and password
         response = client.initiate_auth(
@@ -23,10 +22,13 @@ async def token(login: TokenLogin):
             },
             ClientId=cognito_client_id,
         )
-
         # Get the ID token from the response
         id_token = response['AuthenticationResult']['IdToken']
         expires_in = response['AuthenticationResult']['ExpiresIn']
         return {"id_token": id_token, "expires_in": expires_in}
     except client.exceptions.NotAuthorizedException:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+    except client.exceptions.UserNotFoundException:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.text)
