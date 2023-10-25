fd42284afe788b1067d05c882358e48e06d8968e
nolan1999 <arthur.jaques@hispeed.ch>
2023-10-25 15:01:57 +0200

    Rename GET /files/{file_id} instead of /files/{path} for implementation abstraction

--- workerfacing_api/endpoints/files.py
+++ workerfacing_api/endpoints/files.py
@@ -8,13 +8,13 @@ router = APIRouter()
 s3_client = boto3.client("s3")
 
 
-@router.get("/files/{path:path}/download", status_code=status.HTTP_200_OK)
-async def download_file(path: str, filesystem=Depends(filesystem_dep)) -> str:
-    return filesystem.get_file(path)
+@router.get("/files/{file_id:path}/download", status_code=status.HTTP_200_OK)
+async def download_file(file_id: str, filesystem=Depends(filesystem_dep)) -> str:
+    return filesystem.get_file(path=file_id)
 
 
-@router.get("/files/{path:path}/url", status_code=status.HTTP_200_OK)
+@router.get("/files/{file_id:path}/url", status_code=status.HTTP_200_OK)
 async def url_file(
-    path: str, request: Request, filesystem=Depends(filesystem_dep)
+    file_id: str, request: Request, filesystem=Depends(filesystem_dep)
 ) -> str:
-    return filesystem.get_file_url(path, request.url._url, "/url", "/download")
+    return filesystem.get_file_url(path=file_id, request_url=request.url._url, url_endpoint="/url", files_endpoint="/download")
