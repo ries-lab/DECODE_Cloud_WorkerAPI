a13770184ca035134ba1ab8c136e79ba3b35762d
Arthur Jaques <arthur.jaques@hispeed.ch>
2024-11-19 00:38:42 +0100

    Unify AWS secret getting (#57)

--- workerfacing_api/settings.py
+++ workerfacing_api/settings.py
@@ -1,6 +1,17 @@
 import json
 import os
 
+
+def get_secret_from_env(secret_name: str) -> str | None:
+    secret = os.environ.get(secret_name)
+    if secret:  # exists and not None
+        try:
+            secret = json.loads(secret)["password"]  # AWS Secrets Manager
+        except json.JSONDecodeError:
+            pass
+    return secret
+
+
 # Data
 filesystem = os.environ.get("FILESYSTEM")  # filesystem
 s3_bucket = os.environ.get("S3_BUCKET")
@@ -14,15 +25,9 @@ timeout_failure = int(os.environ.get("TIMEOUT_FAILURE", 300))
 retry_different = bool(int(os.environ.get("RETRY_DIFFERENT", 1)))
 queue_db_url = os.environ.get("QUEUE_DB_URL", "sqlite:///./sql_queue.db")  # RDB queue
 
-if os.environ.get("QUEUE_DB_SECRET"):  # exists and not None
-    queue_db_secret = os.environ["QUEUE_DB_SECRET"]
-    try:
-        queue_db_secret = json.loads(queue_db_secret)["password"]  # AWS Secrets Manager
-    except json.JSONDecodeError:
-        pass
-    queue_db_url = queue_db_url.format(
-        json.loads(os.environ["QUEUE_DB_SECRET"])["password"]
-    )
+queue_db_secret = get_secret_from_env("QUEUE_DB_SECRET")
+if queue_db_secret:
+    queue_db_url = queue_db_url.format(queue_db_secret)
 
 
 # User-facing API
@@ -39,9 +44,4 @@ cognito_user_pool_id = os.environ.get("COGNITO_USER_POOL_ID")
 # default to avoid ConnectionError in tests when initializing `current_user_dep`
 cognito_region = os.environ.get("COGNITO_REGION", "eu-central-1")
 cognito_client_id = os.environ.get("COGNITO_CLIENT_ID")
-cognito_secret = os.environ.get("COGNITO_SECRET")
-if cognito_secret:  # exists and not None
-    try:
-        cognito_secret = json.loads(cognito_secret)["password"]  # AWS Secrets Manager
-    except json.JSONDecodeError:
-        pass
+cognito_secret = get_secret_from_env("COGNITO_SECRET")
